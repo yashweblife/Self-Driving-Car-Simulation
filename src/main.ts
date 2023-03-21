0e1fde3b5d5ebc31ec07b9fda11b0e227a91e017
yashweblife <yashweblife20@gmail.com>
2023-03-20 20:37:31 -0500

    Add animate functions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
+import { Car } from "./lib/Car";
+
 const canvas = document.createElement("canvas")
 document.body.append(canvas);
 canvas.width=500;
 canvas.height=500;
 const ctx = canvas.getContext('2d')!;
+
+
+const car = new Car(10,10,25,50);
+function animate(){
+  ctx.clearRect(0,0,500,500);
+  car.update();  
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
+animate();
