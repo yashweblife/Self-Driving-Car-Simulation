88c696e2b458481c5fd8c8207b455e44ef96c0b7
yashweblife <yashweblife20@gmail.com>
2023-03-20 20:23:43 -0500

    Create car object

---
+++ src/lib/Car.ts
@@ -0,0 +1,9 @@
+class Car{
+    constructor(public x:number,public y:number,public w:number,public h:number){}
+    public draw(c:CanvasRenderingContext2D){
+        c.beginPath();
+        c.rect(this.x-this.w/2, this.y-this.h/2, this.w, this.h);
+        c.fill();
+        c.closePath();
+    }
+}
