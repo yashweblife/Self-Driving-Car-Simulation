f781c52bfe374e0fa474e5414b8dcae6375c860a
yashweblife <yashweblife20@gmail.com>
2023-03-20 20:32:42 -0500

    Add Controls object

---
+++ src/lib/Controls.ts
@@ -0,0 +1,43 @@
+export class Controls {
+  public forward: boolean = false;
+  public left: boolean = false;
+  public back: boolean = false;
+  public right: boolean = false;
+  constructor() {
+    this.addKeyBindings();
+  }
+  private addKeyBindings() {
+    document.addEventListener("keydown", (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowDown":
+          this.back = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+      }
+    });
+    document.addEventListener("keyup", (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowDown":
+          this.back = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+      }
+    });
+  }
+}
