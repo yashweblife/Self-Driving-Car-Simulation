06bcfb36830075bf43bce0ba8538f42c752c923e
yashweblife <yashweblife20@gmail.com>
2023-03-20 21:15:22 -0500

    Change car origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ canvas.height=500;
 const ctx = canvas.getContext('2d')!;
 
 
-const car = new Car(10,10,25,50);
+const car = new Car(canvas.width/2,canvas.height-100,25,50);
 function animate(){
   ctx.clearRect(0,0,500,500);
   car.update();  
