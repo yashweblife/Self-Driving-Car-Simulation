00df35a65a241e3fb949d10a9f86876d123d2ad0
yashweblife <yashweblife20@gmail.com>
2023-03-20 20:35:51 -0500

    Add update functions

--- src/lib/Car.ts
+++ src/lib/Car.ts
@@ -1,4 +1,4 @@
-class Car{
+export class Car{
     constructor(public x:number,public y:number,public w:number,public h:number){}
     public draw(c:CanvasRenderingContext2D){
         c.beginPath();
