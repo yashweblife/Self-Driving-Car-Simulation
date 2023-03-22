bba68ed5ff3a21ad93186a78453106443098981a
yashweblife <yashweblife20@gmail.com>
2023-03-21 23:45:10 -0500

    format

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { Car } from "./lib/Car";
 import "./style.css";
 
-const canvas = document.createElement("canvas")
+const canvas = document.createElement("canvas");
 document.body.append(canvas);
-canvas.width=500;
-canvas.height=window.innerHeight-4;
-const ctx = canvas.getContext('2d')!;
+canvas.width = 500;
+canvas.height = window.innerHeight - 4;
+const ctx = canvas.getContext("2d")!;
 
-
-const car = new Car(canvas.width/2,canvas.height-100,25,50);
-function animate(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  car.update();  
+const car = new Car(canvas.width / 2, canvas.height - 100, 25, 50);
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  car.update();
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
