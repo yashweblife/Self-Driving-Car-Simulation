9ff4b64c147145f30fa2c4777360a9c0cc5a3626
yashweblife <yashweblife20@gmail.com>
2023-03-21 23:43:49 -0500

    fixed clearing rect

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const ctx = canvas.getContext('2d')!;
 
 const car = new Car(canvas.width/2,canvas.height-100,25,50);
 function animate(){
-  ctx.clearRect(0,0,500,500);
+  ctx.clearRect(0,0,canvas.width,canvas.height);
   car.update();  
   car.draw(ctx);
   requestAnimationFrame(animate);
