0cc0571d8ae1282b62949a0a40772defe6405282
yashweblife <yashweblife20@gmail.com>
2023-03-21 23:35:44 -0500

    create vector object

---
+++ src/lib/Vector.ts
@@ -0,0 +1,33 @@
+export class Vector {
+  constructor(
+    public x: number = 0,
+    public y: number = 0,
+    public z: number = 0
+  ) {}
+  public add = (v: Vector) => {
+    this.x += v.x;
+    this.y += v.x;
+    this.z += v.x;
+  };
+  public subtract = (v: Vector) => {
+    this.x -= v.x;
+    this.y -= v.x;
+    this.z -= v.x;
+  };
+  public multiply = (v: Vector) => {
+    this.x *= v.x;
+    this.y *= v.x;
+    this.z *= v.x;
+  };
+  public divide = (v: Vector) => {
+    this.x /= v.x;
+    this.y /= v.x;
+    this.z /= v.x;
+  };
+  public lerp = (v: Vector, t: number) => {
+    const x = this.x + (v.x - this.x) * t;
+    const y = this.y + (v.y - this.y) * t;
+    const z = this.z + (v.z - this.z) * t;
+    return new Vector(x, y, z);
+  };
+}
