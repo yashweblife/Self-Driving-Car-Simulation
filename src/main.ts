f0a6c416d4241cf3886529144f452b2a9a2e3815
yashweblife <yashweblife20@gmail.com>
2023-03-22 23:23:33 -0500

    impliment road

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { Car } from "./lib/Car";
+import { Road } from "./lib/Road";
+import { Vector } from "./lib/Vector";
 import "./style.css";
 
 const canvas = document.createElement("canvas");
@@ -6,11 +8,12 @@ document.body.append(canvas);
 canvas.width = 500;
 canvas.height = window.innerHeight - 4;
 const ctx = canvas.getContext("2d")!;
-
-const car = new Car(canvas.width / 2, canvas.height - 100, 25, 50);
+const road = new Road(new Vector(canvas.width/2, canvas.height/2,0),500-10);
+const car = new Car(road.getLaneCenter(3),200, 25, 50);
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   car.update();
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
