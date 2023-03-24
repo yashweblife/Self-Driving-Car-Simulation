d993f900a9cac1682980479dbb13d7937be61385
yashweblife <yashweblife20@gmail.com>
2023-03-24 17:28:10 -0500

    Add Line Class

--- src/lib/Vector.ts
+++ src/lib/Vector.ts
@@ -34,3 +34,33 @@ export class Vector {
     return this.x + (this.y - this.x) * this.z;
   };
 }
+export function lerp(a: number, b: number, t: number) {
+  return a + (b - a) * t;
+}
+export class Line {
+  public points: boolean = false;
+  constructor(public start: Vector, public end: Vector) {}
+  public findIntersect = (l: Line) => {
+    const topT =
+      (l.end.x - l.start.x) * (this.start.y - l.start.y) -
+      (l.end.y - l.start.y) * (this.start.x - l.start.x);
+    const topU =
+      (l.start.y - this.start.y) * (this.start.x - this.end.x) -
+      (l.start.x - this.start.x) * (this.start.y - this.end.y);
+    const bottom =
+      (l.end.y - l.start.y) * (this.end.x - this.start.x) -
+      (l.end.x - l.start.x) * (this.end.y - this.start.y);
+    if (bottom) {
+      const t = topT / bottom;
+      const u = topU / bottom;
+      if (t > 0 && t <= 1 && u > 0 && u <= 1) {
+        console.log(t);
+        return new Vector(
+          lerp(this.start.x, this.end.x, t),
+          lerp(this.start.y, this.end.y, t)
+        );
+      }
+    }
+    return null;
+  };
+}
