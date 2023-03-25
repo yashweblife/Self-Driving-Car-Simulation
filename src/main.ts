1912034b23ff2558d1a4312bf8f0be662cd888d7
yashweblife <yashweblife20@gmail.com>
2023-03-24 20:06:04 -0500

    add some borders

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ canvas.width = 500;
 canvas.height = window.innerHeight - 4;
 const ctx = canvas.getContext("2d")!;
 const road = new Road(new Vector(canvas.width/2, canvas.height/2,0),500-10);
-const car = new Car(road.getLaneCenter(3),200, 25, 50);
+const car = new Car(road.getLaneCenter(1),200, 25, 50);
 function animate() {
   ctx.save();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  car.update();
+  car.update(road.borders);
   ctx.translate(0, -car.y+canvas.height*0.8)
   road.draw(ctx);
   car.draw(ctx);
