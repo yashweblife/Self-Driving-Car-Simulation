7effe867ecc273c818e063f89479f94330832f9d
yashweblife <yashweblife20@gmail.com>
2023-04-12 21:33:16 -0500

    add various stuff

--- src/lib/Car.ts
+++ src/lib/Car.ts
@@ -1,4 +1,5 @@
 import { Controls } from "./Controls";
+import { Network } from "./Network";
 import { Sensor } from "./Sensor";
 import { Line, Vector } from "./Vector";
 import { polyIntersect, polyPolyIntersect } from "./utils";
@@ -14,23 +15,38 @@ export class Car {
   public angle: number = 0;
   public friction: number = 0.05;
   public maxSpeed: number = 3;
-  public sensor: (Sensor|null);
+  public sensor: Sensor | null;
   public polygons: Vector[] = [];
   public damaged: boolean = false;
-  constructor(x: number, y: number, public w: number, public h: number, type:string, maxSpeed:number=3) {
+  public brain: Network | null;
+  public useBrain = false;
+  public color = "rgba(0,0,0,0.2)";
+  public drawSensor = false;
+  constructor(
+    x: number,
+    y: number,
+    public w: number,
+    public h: number,
+    type: string,
+    maxSpeed: number = 3
+  ) {
     this.x = x;
     this.y = y;
     this.polygons = this.createPolygon();
+    this.useBrain = type == "AI";
     this.controls = new Controls(type);
-    this.maxSpeed=maxSpeed;
-    if(type!="DUMMY"){
+    this.maxSpeed = maxSpeed;
+    if (type != "DUMMY") {
       this.sensor = new Sensor(this);
-    }else{
-      this.sensor=null
+      this.brain = new Network([this.sensor.rayCount,  6, 4]);
+    } else {
+      this.color = "red";
+      this.sensor = null;
+      this.brain = null;
     }
   }
   public draw(c: CanvasRenderingContext2D) {
-    if(this.sensor){
+    if (this.sensor && this.drawSensor) {
       this.sensor.draw(c);
     }
     c.beginPath();
@@ -39,18 +55,28 @@ export class Car {
       c.lineTo(this.polygons[i].x, this.polygons[i].y);
     }
     c.closePath();
-    if(this.damaged){
-      c.fillStyle = "red"
-    }else{
-      c.fillStyle = "black";
+    if (this.damaged) {
+      c.fillStyle = "rgba(0,255,0,0.5)";
+    } else {
+      c.fillStyle = this.color;
     }
     c.fill();
   }
-  public update = (borders: Line[], traffic:Car[]) => {
+  public update = (borders: Line[], traffic: Car[]) => {
     this.accessDamage(borders, traffic);
-    if(this.damaged) return
-    if(this.sensor){
+    if (this.sensor) {
       this.sensor.update(borders, traffic);
+      const offset = this.sensor.intersections.map((val: Vector | null) => {
+        if (val == null) return 0;
+        return 1 - val.z;
+      });
+      const output = Network.feedForward(offset, this.brain!);
+      if (this.useBrain) {
+        this.controls.forward = output[0] == 1;
+        this.controls.left = output[1] == 1;
+        this.controls.back = output[2] == 1;
+        this.controls.right = output[3] == 1;
+      }
     }
     this.polygons = this.createPolygon();
     if (this.controls.forward) {
@@ -87,10 +113,16 @@ export class Car {
     this.x -= Math.sin(this.angle) * this.vy;
     this.y -= Math.cos(this.angle) * this.vy;
   };
-  public accessDamage = (borders: Line[],traffic:Car[]) => {
+  public accessDamage = (borders: Line[], traffic: Car[]) => {
     this.damaged = polyIntersect(this.polygons, borders);
-    for(let i=0;i<traffic.length;i++){
+    for (let i = 0; i < traffic.length; i++) {
       this.damaged = polyPolyIntersect(this.polygons, traffic[i].polygons);
+    }
+    if (this.damaged) {
+      this.ax = 0;
+      this.ay = 0;
+      this.vx = 0;
+      this.vy = 0;
     }
   };
   private createPolygon = () => {
@@ -125,5 +157,3 @@ export class Car {
     return points;
   };
 }
-
-
